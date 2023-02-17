794542f6ffc1873e0f2904c053f97ddb4b250644
Kim Young Hun <114043149+gns253@users.noreply.github.com>
2023-02-17 23:57:59 +0900

    Update 순서쌍의 개수.java
    코드 수정

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 1; i < n+1; i++) {
+        for(int i = 1; i <= n; i++) {
             if(n%i==0) {
                 answer++;
             }
